53fc318fbd09c4fb0692b09447efceb46b23931e
Thanoj Singh <thanoj.singh@gmail.com>
2016-10-23 15:23:58 -0400

    Add invalid sign in tests and controller functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
-  resources :sesions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
 
