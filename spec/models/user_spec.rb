fefa8f122bd89ae2656cb9ddcdd9f3f70f6532e0
Thanoj Singh <thanoj.singh@gmail.com>
2016-12-05 01:05:47 -0500

    Page 449

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,15 +34,23 @@ describe User do
 
     before { @user.save}
     let!(:older_micropost) do
-      FacotoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
 
-    let!(:new_micropost) do
-      FacotoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
 
     it "should have the right microposts in the right order" do
       @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
     end
   end
 
